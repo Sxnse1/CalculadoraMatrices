68cef586637da3c6d3bbdf039a1317caa9f52ae3
César Alejandro Dávila Reyna <126361906+Sxnse1@users.noreply.github.com>
2025-02-10 13:05:20 -0600

    fuga

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+
+
+function confirmarMatriz() {
+
 let filasMatriz = document.getElementById('filasMatriz').value;
-let columnasMatriz
+let columnasMatriz = document.getElementById('columnasMatriz').value;
+
+
+console.log(filasMatriz + " y " + columnasMatriz);
+
+document.getElementById('div-inicial').style.display = 'none';
+document.getElementById('div-matriz').style.display = 'flex';  
+
+}
